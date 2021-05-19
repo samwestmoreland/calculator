6ca338cd48be08f01b4329ffefd197558fd4a4a2
Sam Westmoreland <samwestmoreland@gmail.com>
2021-05-19 22:01:57 +0100

    end of chapter 6

--- calculator.cpp
+++ calculator.cpp
@@ -2,40 +2,141 @@
 
 using namespace std;
 
-int main()
+class Token
 {
-    cout << "Please enter expression (we can handle +, -, *, and /)" << endl;
-    cout << "Add an x to end expression (e.g. 1+2x): ";
-    int lval = 0;
-    int rval;
-    cin >> lval;
-    if (!cin) error("no first operand");
+    public:
+        char kind;      // what kind of token
+        double value;   // for numbers: a value
+};
 
-    // Repeatedly read in "operator followed by operand"
-    for (char op; cin >> op;)
-    {
-        if (op != 'x') cin >> rval;
-        if (!cin) error("no second operand");
-        switch (op)
-        {
-            case '+':
-                lval += rval;
-                break;
-            case '-':
-                lval -= rval;
-                break;
+class Token_stream {
+    public:
+        Token get();
+        void putback(Token t);
+    private:
+        bool full {false};  // is there a token in this buffer?
+        Token buffer;       // keep a token put back using putback()
+};
+
+void Token_stream::putback(Token t)
+{
+    if (full) error("putback() into a full buffer");
+    buffer = t;     // copy t to buffer
+    full = true;    // buffer is now full
+}
+
+Token Token_stream::get()
+{
+    if (full) {
+        full = false;   // remove token from buffer
+        return buffer;
+    }
+
+    char ch;
+    cin >> ch;
+    switch (ch) {
+        case ';':   // for "print"
+        case 'q':   // for "quit"
+        case '(': case ')': case '+': case '-': case '*': case '/':
+            return Token{ch};
+        case '.':
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+            {
+                cin.putback(ch);    // put digit back into input stream
+                double val;
+                cin >> val;         // read a floating point number
+                return Token{'8', val};
+            }
+        default:
+            error("Bad token");
+    }
+}
+Token_stream ts;
+double expression();
+double primary()
+{
+    Token t = ts.get();
+    switch (t.kind) {
+        case '(':   // handle '(' expression ')'
+            {
+                double d = expression();
+                t = ts.get();
+                if (t.kind != ')') error("expected ')'");
+                return d;
+            }
+        case '8':
+            return t.value;
+        default:
+            error("primary expected");
+    }
+}
+
+double term()
+{
+    double left = primary();
+    Token t = ts.get();     // get the next token from the token stream
+    while (true) {
+        switch (t.kind) {
             case '*':
-                lval *= rval;
+                left *= primary();
+                t = ts.get();
                 break;
             case '/':
-                lval /= rval;
-                break;
+                {
+                    double d = primary();
+                    if (d == 0) error("divide by zero");
+                    left /= d;
+                    t = ts.get();
+                    break;
+                }
             default:
-                cout << "Result: " << lval << endl;
-                return 0;
+                ts.putback(t);
+                return left;
         }
     }
-    error("bad expression");
+}
 
-    return 0;
+double expression()
+{
+    double left = term();   // read and evaluate a Term
+    Token t = ts.get();     // get the next token from the token stream
+    while (true) {
+        switch (t.kind) {
+            case '+':
+                left += term();
+                t = ts.get();
+                break;
+            case '-':
+                left -= term();
+                t = ts.get();
+                break;
+            default:
+                ts.putback(t);
+                return left;
+        }
+    }
+}
+
+
+
+int main()
+try {
+    double val = 0;
+    while (cin) {
+        Token t = ts.get();
+        if (t.kind == 'q') break;
+        if (t.kind == ';') cout << "=" << val << endl;
+        else
+            ts.putback(t);
+        val = expression();
+    }
+}
+catch (exception& e) {
+    cerr << e.what() << endl;
+    return 1;
+}
+catch (...) {
+    cerr << "exception" << endl;
+    return 2;
 }
